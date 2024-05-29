12a77e0dd0b831c2543a21b77ca3b0eb9a4ef2ff
Ameh Lawal <amehlawal@gmail.com>
2024-05-29 16:01:03 +0100

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+    //this is a comment
+    int age = 21;
+    int year = 2034;
+    int days = 7.5;
+
+    // single character
+
+    char initial = 'a';
+    char grade = 'B';
+
+    // boolean
+    
+    std::cout << "I like Pizza" << '\n';
+    std::cout << "It is really good" << '\n';
+    std::cout << year;
+    return 0;
+}
