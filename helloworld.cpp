090c3ebdc381d7dcdb8fd2a842a0df3fd8b097a5
Ameh Lawal <amehlawal@gmail.com>
2024-05-29 16:15:27 +0100

    calculate circumference

--- helloworld.cpp
+++ helloworld.cpp
@@ -13,9 +13,22 @@ int main(){
     char grade = 'B';
 
     // boolean
-    
+    bool student = false;
+
+    // string
+    std::string name = "Lawal Ameh";
     std::cout << "I like Pizza" << '\n';
     std::cout << "It is really good" << '\n';
-    std::cout << year;
+
+
+    std:: cout << "Hello " << name << '\n';
+    std:: cout << "you are " << age << '\n';
+
+    // const
+    const double pi = 3.141;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    std:: cout << circumference << '\n';
     return 0;
 }
